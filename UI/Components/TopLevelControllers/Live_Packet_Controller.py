63d3b8347026ad7025d060ebad3917d5ffaffe23
Jerry U👻❤ <juranga@github.com>
2019-04-28 17:40:41 -0600

    trying to get this gui app to work

--- UI/Components/TopLevelControllers/Live_Packet_Controller.py
+++ UI/Components/TopLevelControllers/Live_Packet_Controller.py
@@ -5,7 +5,7 @@ from Infrastructure.CaptureLibrary.Proxy_Server import Proxy_Server
 class Live_Packet_Controller():
 
     def __init__(view = Live_Packet_View(), proxy_server = Proxy_Server()):
-        self.view = view
+        self.proxy_toggle = None
+        self.view =  Live_Packet_View(self.proxy_toggle)
         self.proxy_server = proxy_server
-        self.view.proxy_toggle = Proxy_Toggle_Component(view.horizontalFrame_6, view.sizePolicy, view.font, proxy_server = self.proxy_server)
-        self.view.horizontalLayout_7.addWidget(self.proxy_toggle)
+        #self.view.proxy_toggle = Proxy_Toggle_Component(proxy_server = self.proxy_server)
