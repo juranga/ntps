1733bcff4306db4331b3503cc9df7456678a1d5c
jerry <urangagerardo@gmail.com>
2019-04-29 09:54:34 -0600

    Proxy Toggle Component if change switch

--- UI/Components/TopLevelControllers/LivePacketComponents/Proxy_Toggle_Component.py
+++ UI/Components/TopLevelControllers/LivePacketComponents/Proxy_Toggle_Component.py
@@ -10,14 +10,14 @@ class Proxy_Toggle_Component():
 
     def install_widgets(self, parent, sizePolicy, font):
         self.combo_box = QtWidgets.QComboBox(parent)
-        self.combo_box.addItems(["Enabled/Disabled", "Enabled", "Disabled"])
+        self.combo_box.addItems(["Enabled", "Disabled"])
         self.combo_box.setCurrentText("Enabled/Disabled")
         self.combo_box.currentIndexChanged.connect(self.selection_change)
-        self.combo_box.setMaxVisibleItems(3)
+        self.combo_box.setMaxVisibleItems(2)
 
     # TODO: Needs more testing.
     def selection_change(self, idx):
-        if not self.combo_box.currentText() == "Enabled":
-            self.proxy_server.stop_server()
-        elif self.combo_box.currentText() == "Enabled":
+        if self.combo_box.currentText() == "Enabled":
             self.proxy_server.init_server()
+        else:
+            self.proxy_server.stop_server()
