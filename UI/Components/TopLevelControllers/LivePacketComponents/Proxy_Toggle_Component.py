167a0ba965708c066148cb093358d9eaee9e0b89
jerry <urangagerardo@gmail.com>
2019-05-02 13:15:18 -0600

    removing the jerry branch change on master

--- UI/Components/TopLevelControllers/LivePacketComponents/Proxy_Toggle_Component.py
+++ UI/Components/TopLevelControllers/LivePacketComponents/Proxy_Toggle_Component.py
@@ -7,7 +7,6 @@ class Proxy_Toggle_Component():
 
     def __init__(self, proxy_server= Proxy_Server()):
         self.proxy_server = proxy_server
-        self.thread = Thread(target=self.proxy_server.init_server())
 
     def install_widgets(self, parent, sizePolicy, font):
         self.combo_box = QtWidgets.QComboBox(parent)
@@ -19,7 +18,6 @@ class Proxy_Toggle_Component():
     # TODO: Needs more testing.
     def selection_change(self, idx):
         if self.combo_box.currentText() == "Enabled":
-            self.thread.start()
+            self.proxy_server.init_server()
         else:
             self.proxy_server.stop_server()
-            self.thread.join()
