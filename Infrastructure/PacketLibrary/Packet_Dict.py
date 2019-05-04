d2479590f6620df62f770907257ce8b40bfe4dac
Jerry U👻❤ <juranga@github.com>
2019-05-03 22:10:13 -0600

    gg2?

--- Infrastructure/PacketLibrary/Packet_Dict.py
+++ Infrastructure/PacketLibrary/Packet_Dict.py
@@ -7,19 +7,17 @@ Author: Zhi Yuan
 from scapy.all import *
 from cStringIO import StringIO
 import sys
+
 class Capturing(list):
-        """
-        This class will capture sys.out.
-        More info:
-        http://stackoverflow.com/questions/16571150/how-to-capture-stdout-output-from-a-python-function-call
-        """
+    
     def __enter__(self):
         self._stdout = sys.stdout
         sys.stdout = self._stringio = StringIO()
         return self
+        
     def __exit__(self, *args):
         self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
+        del self._stringio# free up some memory
         sys.stdout = self._stdout
 
 class PacketDict(dict):
