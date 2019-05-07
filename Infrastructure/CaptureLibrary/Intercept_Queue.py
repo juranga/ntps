7927a139f723a934017122b2ec6199687b6830d2
root <root@localhost.localdomain>
2019-05-06 22:01:56 -0400

    making it look more like the SRS

--- Infrastructure/CaptureLibrary/Intercept_Queue.py
+++ Infrastructure/CaptureLibrary/Intercept_Queue.py
@@ -1,5 +1,6 @@
 from Infrastructure.Common.Generators import id_generator
-from Infrastructure.PacketLibrary.Packet import Packet
+from Infrastructure.PacketLibrary.Packet import Dissected_Packet
+from Infrastructure.PacketLibrary.Dissector import Dissector
 
 from PyQt5.QtGui import QStandardItemModel, QStandardItem, QIcon
 from PyQt5.QtCore import QThread
@@ -18,6 +19,8 @@ class Intercept_Queue:
 
     def populate_gui(self):
         print('Populating to GUI...', flush=True)
+        layer_dict = self.packet_list[-1].layer_dict
+
         parent = QStandardItem(QIcon(arrow), 
                 "Frame {}, {}".format(id_generator(size=3), ', '.join(self.packet_list[-1].layers)
             ))
@@ -25,16 +28,17 @@ class Intercept_Queue:
         self.model.appendRow(parent)
 
         for layer in self.packet_list[-1].layers:
+            display_layer = layer
+            if layer in layer_dict:
+                display_layer = layer_dict[layer]
             if "src" in self.packet_list[-1].fields[layer] and "dst" in self.packet_list[-1].fields[layer]:
                 src = self.packet_list[-1].fields[layer]["src"]
                 dst = self.packet_list[-1].fields[layer]["dst"]
                 child = QStandardItem(QIcon(circle),
-                    "{}, Src:{}, Dst:{}".format(layer, )
-                    ))
+                    "{}, Src:{}, Dst:{}".format(display_layer, src, dst))
             else:
                 child = QStandardItem(QIcon(circle),
-                    "{}".format(self.packet_list[-1].layer_dict[layer]
-                    ))
+                    "{}".format(display_layer))
             child.setEditable(False)
             self.model.itemFromIndex(self.model.indexFromItem(parent)).appendRow(child)
         
@@ -47,15 +51,14 @@ class Intercept_Queue:
 
     def dissect_put(self, packet, idx = -1, icon=arrow):
         packet_layers = self.packet_list[-1].layers
-        layer_dict = self.packet_list[-1].layer_dict
         packet_fields = self.packet_list[-1].fields
         for key, value in packet.items():
             if type(value) is dict:
-                packet_layers.append(layer_dict[key])
-                self.put(value, idx+1, circle)
+                packet_layers.append(key)
+                self.dissect_put(value, idx+1, circle)
                 break
             else:
-                packet_fields[packet_layers[idx]][layer_dict[key]] = value
+                packet_fields[packet_layers[idx]][key] = value
 
     def get(self):
         packet = self.packet_list[0]
