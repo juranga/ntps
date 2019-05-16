e281859317c4926cac8dd8495232a1bcbab8fc7b
Taylor Dodson <t.raynedod@gmail.com>
2019-05-16 02:31:49 -0400

    Can now change queue size!

--- UI/Components/TopLevelControllers/Live_Packet_Controller.py
+++ UI/Components/TopLevelControllers/Live_Packet_Controller.py
@@ -7,6 +7,7 @@ from UI.Components.TopLevelControllers.LivePacketComponents.Field_Area_Component
 from UI.Components.TopLevelControllers.LivePacketComponents.Forward_Button import Forward_Button
 from UI.Components.TopLevelControllers.LivePacketComponents.Save_Button import Save_Button
 from UI.Components.TopLevelControllers.LivePacketComponents.Drop_Button import Drop_Button
+from UI.Components.TopLevelControllers.LivePacketComponents.Queue_Size_Component import Queue_Size_Component
 
 from UI.Components.View.Live_Packet_View import Live_Packet_View
 from Infrastructure.CaptureLibrary.Proxy_Server import Proxy_Server
@@ -24,8 +25,10 @@ class Live_Packet_Controller():
         self.save_button = Save_Button(proxy_server, self.field_area)
         self.drop_button = Drop_Button(proxy_server, self.field_area)
         self.forward_button = Forward_Button(proxy_server, self.field_area)
+        self.queue_edit = Queue_Size_Component(proxy_server)
+        
         self.view =  Live_Packet_View(self.proxy_toggle, self.intercept_toggle,
                                       self.packet_area, self.packet_binary_area,
                                       self.packet_hex_area, self.field_area,
                                       self.forward_button, self.save_button,
-                                      self.drop_button)
+                                      self.drop_button, self.queue_edit)
